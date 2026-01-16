2c4882646d42255c9d5a92d34ad719b55618ccd3
Tatyana <filippovat42@gmail.com>
2026-01-16 11:04:35 +0300

    TimeCalendar

--- src/components/TimeCalendar/TimeCalendar.tsx
+++ src/components/TimeCalendar/TimeCalendar.tsx
@@ -1,6 +1,7 @@
 import style from "./styles.module.scss";
 import heart from "../../../public/heart.svg";
 import lineVertical from "../../../public/lineVertical.svg";
+import heartSmall from "../../../public/heartSmall.svg";
 
 const TimeCalendar = () => {
   return (
@@ -18,7 +19,34 @@ const TimeCalendar = () => {
         <div className={style.sizeSmall}>16</div>
       </div>
       <div className={style.wrapperTime}>
-        sdf
+        <div className={style.shellFirst}>
+          <div className={style.wrapperTimeText}>
+            <div className={style.textTime}>16:30</div>
+            <div className={style.textTime}>сбор гостей</div>
+          </div>
+          <img src={heartSmall} className={style.heartSmall} />
+        </div>
+        <div className={style.shellFirst}>
+          <div className={style.wrapperTimeText}>
+            <div className={style.textTime}>17:00</div>
+            <div className={style.textTime}>торжественная церемония</div>
+          </div>
+          <img src={heartSmall} className={style.heartSmall} />
+        </div>
+        <div className={style.shellFirst}>
+          <div className={style.wrapperTimeText}>
+            <div className={style.textTime}>18:00</div>
+            <div className={style.textTime}>начало банкета</div>
+          </div>
+          <img src={heartSmall} className={style.heartSmall} />
+        </div>
+        <div className={style.shellFirst}>
+          <div className={style.wrapperTimeText}>
+            <div className={style.textTime}>21:00</div>
+            <div className={style.textTime}>праздничный торт </div>
+          </div>
+          <img src={heartSmall} className={style.heartSmall} />
+        </div>
         <img src={lineVertical} className={style.lineVertical} />
       </div>
     </div>
